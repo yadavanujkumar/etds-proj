1a9841d86e7dcf0484e5dc2c3f275bbb6de21505
yadavanujkumar <thunderstruck9999@gmail.com>
2025-02-26 22:52:31 +0530

    requirements

--- app2.py
+++ app2.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 import shap  # Import SHAP for model explainability
 from sklearn.linear_model import LinearRegression
 # -----------------------------
